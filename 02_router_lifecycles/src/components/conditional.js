f0d88b481c36b4fd5cbe1911253c3865b45e5fcb
rozbeh1212 <35690647+rozbeh1212@users.noreply.github.com>
2021-08-12 15:18:59 +0430

    react-route

---
+++ 02_router_lifecycles/src/components/conditional.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Conditional = () => {
+
+    //const value = false;
+
+    const returnValue = () => {
+        /////
+        return true
+    }
+
+    const showIt = () => {
+        return ( returnValue() ?
+            <div>
+                Hello, it's true
+            </div> 
+            :
+            <div>
+                Hello, it's false
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            { showIt()}
+        </div>
+    )
+}
+
+export default Conditional;
